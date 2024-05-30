573eff9fac4c2ec9e8fe7fb0e85669b6931c1c43
yeeun03030 <skdPdms04@gmail.com>
2024-05-30 16:01:41 +0900

    Activity 추가 1

---
+++ app/src/main/java/kr/ac/kopo/addactivitytest2024/MainActivity.kt
@@ -0,0 +1,21 @@
+package kr.ac.kopo.addactivitytest2024
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btnMain = findViewById<Button>(R.id.btnMain)
+        btnMain.setOnClickListener {
+            var intent = Intent(applicationContext, SecondActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/kr/ac/kopo/addactivitytest2024/SecondActivity.kt
@@ -0,0 +1,17 @@
+package kr.ac.kopo.addactivitytest2024
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        var btnSecond = findViewById<Button>(R.id.btnSecond)
+        btnSecond.setOnClickListener {
+            finish()
+        }
+    }
+}
